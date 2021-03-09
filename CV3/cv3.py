7d98bb300e2092cccfc46dfa46f0c34a05b071ff
Peter Zbin <xzbin@stuba.sk>
2021-03-09 06:39:36 +0100

    feat: implementácia ukladania výsledkov a grafu
    funkcia plot_graph a ukladanie výsledku. Funkciu mám zo zdroja - seminár

--- CV3/cv3.py
+++ CV3/cv3.py
@@ -1,6 +1,7 @@
 from fei.ppds import Mutex, Semaphore, Thread
 from time import sleep
 from random import randint
+from matplotlib import pyplot as plt
 
 
 class Shared:
@@ -41,7 +42,7 @@ def producer(shared, th_id, prod_time):
 
         shared.producing_done.signal()
 
-        if shared.finished:
+        if shared.finished:  # ak experiment skoncil
             break
 
 
@@ -55,7 +56,7 @@ def consumer(shared, th_id, consume_time):
         shared.mutex.unlock()
         shared.consuming_done.signal()
 
-        if shared.finished:
+        if shared.finished:  # ak experiment skoncil
             break
 
 
@@ -65,19 +66,23 @@ def experiment():       # zdrojom je seminar, z neho inspiracia riesenia
     results = []
     cons_time = 0.0005
     n_consumers = 50
-    sleep_time = 0.005
+    sleep_time = 0.005       # vnutorny cyklus caka sleep_time a zastavi vlakna
 
-    for prod_time in range(1, 5):  # menim cas produkcie jednej polozky
+    for prod_time in range(1, 5):
+        # menim cas produkcie jednej polozky
         prod_time = prod_time / 100
 
         for prod in range(1, 30):
+            iters_per_second_sum = 0
 
             for i in range(reps):
+                # 10 krat urobim to iste, spriemerujem hodnoty a ulozim
 
                 shared = Shared()
                 producers = create_threads(prod, shared, prod_time, producer)
                 consumers =
                 create_threads(n_consumers, shared, cons_time, consumer)
+
                 sleep(sleep_time)
                 shared.finished = True
 
@@ -90,6 +95,13 @@ def experiment():       # zdrojom je seminar, z neho inspiracia riesenia
                 for thread in consumers:
                     thread.join()
 
+                items_per_second = shared.items_count / sleep_time
+                iters_per_second_sum += items_per_second
+
+            average_ips = iters_per_second_sum / reps
+            results.append((prod_time, prod, average_ips))
+    plot_graph(results)
+
 
 def create_threads(num_producers, shared, prod_time, fnc):
 
@@ -99,6 +111,23 @@ def create_threads(num_producers, shared, prod_time, fnc):
         threads.append(Thread(fnc, shared, i, prod_time))
 
     return threads
+
+
+def plot_graph(graph_data):     # funkcia na zaklade seminaru
+
+    fig = plt.figure()
+    ax = plt.axes(projection="3d")
+    x = [item[0] for item in graph_data]
+    y = [item[1] for item in graph_data]
+    z = [item[2] for item in graph_data]
+
+    ax.set_xlabel("čas produkcie")
+    ax.set_ylabel("počet producentov")
+    ax.set_zlabel("počet produktov / sekunda")
+
+    ax.plot_trisurf(x, y, z, cmap='viridis', edgecolor='none')
+
+    plt.show()
 
 
 if __name__ == '__main__':
