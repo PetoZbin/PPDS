fdb4b4874d7549259e667acc6b91fdf5743f40dd
Peter Zbin <xzbin@stuba.sk>
2021-03-29 16:07:24 +0200

    feat: kostra programu problemu h2o
    zakladne funkcie bond, hydrogen, oxygen, trieda Shared

---
+++ CV6/voda.py
@@ -0,0 +1,25 @@
+class Shared:
+
+    def __init__(self):
+
+        pass
+
+
+def oxygen(shared):
+
+    pass
+
+
+def hydrogen(shared):
+
+    pass
+
+
+def bond(shared):
+
+    pass
+
+
+if __name__ == '__main__':
+
+    shared = Shared()
