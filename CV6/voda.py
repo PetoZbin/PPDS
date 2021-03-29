96cf4dff50e21ebc91b1b8ec4e4a28f4bed1250e
Peter Zbin <xzbin@stuba.sk>
2021-03-29 17:24:48 +0200

    feat: implementácia hydrogen, oxygen, Shared
    implementácia daných funkcii podľa zdroja - prednáška

--- CV6/voda.py
+++ CV6/voda.py
@@ -1,18 +1,65 @@
+from fei.ppds import Semaphore, Thread, Mutex, print
+from time import sleep
+
+
 class Shared:
 
     def __init__(self):
 
+        self.molecules_formed = 0
+        self.actual_molecule_bonds = 0  # naratava do 3 vazieb pre molekulu
+
+        self.oxygens_available = 0
+        self.hydrogens_available = 0
+        self.oxyQueue = Semaphore(0)  # semafory, kde cakaju kysliky / vodiky
+        self.hydroQueue = Semaphore(0)
+        self.mutex = Mutex()
+        self.barrier = Semaphore(3)
         pass
 
+    def consume_atoms(self):
+        self.oxygens_available -= 1
+        self.hydrogens_available -= 2
 
-def oxygen(shared):
 
-    pass
+def oxygen(shared):     # kod hydrogen a oxygen na zaklade prednasky
+
+    sleep(0.001)
+    shared.mutex.lock()
+
+    shared.oxygens_available += 1
+
+    if shared.hydrogens_available < 2:
+        shared.mutex.unlock()
+    else:
+        shared.consume_atoms()
+        shared.oxyQueue.signal()
+        shared.hydroQueue.signal(2)
+
+    shared.oxyQueue.wait()
+    bond(shared)
+
+    shared.barrier.wait()
+    shared.mutex.unlock()
 
 
 def hydrogen(shared):
 
-    pass
+    sleep(0.001)
+    shared.mutex.lock()
+    shared.hydrogens_available += 1
+
+    if (shared.oxygens_available < 1) or (shared.hydrogens_available < 2):
+        shared.mutex.unlock()
+    else:
+        shared.consume_atoms()
+        shared.oxyQueue.signal()
+        shared.hydroQueue.signal(2)
+
+    shared.hydroQueue.wait()
+    bond(shared)
+
+    shared.barrier.wait()
 
 
 def bond(shared):
